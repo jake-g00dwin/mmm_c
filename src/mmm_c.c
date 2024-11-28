bf2f32fb7f0d4be7f52c43be5dfa002f3c1c27ce
jakeg00dwin <jakegoodwin@gorge.works>
2024-11-27 21:22:05 -0800

    Added some funciton prototypes and defs.

--- src/mmm_c.c
+++ src/mmm_c.c
@@ -1,1 +1,30 @@
+#include "mmm_c.h"
+#include <alloca.h>
 
+U16_DMAT DU16_NewZeros(uint16_t rows, uint16_t cols) {
+  U16_DMAT mat;
+  mat.rows = rows;
+  mat.cols = cols;
+
+  if (DU16_AllocateMemory(&mat) != Ok) {
+    mat.cells = NULL;
+  }
+
+  return mat;
+}
+
+Result DU16_AllocateMemory(U16_DMAT *mat) {
+  if (!calloc(mat->cols * mat->rows, sizeof(uint16_t))) {
+    return Error;
+  }
+  return Ok;
+}
+
+Result DU16_FreeMatrix(U16_DMAT *mat) {
+  if (mat->cells == NULL) {
+    return Error;
+  }
+
+  free(mat->cells);
+  return Ok;
+}
---
+++ src/mmm_c.h
@@ -0,0 +1,41 @@
+/**
+ * @file mmm_c.h
+ * @author Jake G
+ * @date 27 Nov 2024
+ * @brief File contains the public API for the MMM_C library.
+ *
+ * This file holds all the code/functions needed to preform matrix operations.
+ *
+ */
+#ifndef MMMC_H
+#define MMMC_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef enum { Ok, Error } Result;
+
+/**
+ * @brief Unsigned 16bit dynamic matrix structure
+ *
+ */
+typedef struct {
+  uint16_t rows;
+  uint16_t cols;
+  uint16_t *cells;
+} U16_DMAT;
+
+/**
+ * @brief summaryhere
+ *
+ */
+
+U16_DMAT DU16_NewZeros(uint16_t rows, uint16_t cols);
+U16_DMAT DU16_NewOnes(uint16_t rows, uint16_t cols);
+U16_DMAT DU16_NewIdentity(uint16_t rows, uint16_t cols);
+
+Result DU16_AllocateMemory(U16_DMAT *mat);
+Result DU16_FreeMatrix(U16_DMAT *mat);
+Result DU16_MultiplyInPlace(U16_DMAT *mata, U16_DMAT *matb);
+
+#endif // MMMC_H
