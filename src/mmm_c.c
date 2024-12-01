9ae23c17440aad1d87ca86c054288162dd1b93b7
jakeg00dwin <jakegoodwin@gorge.works>
2024-11-30 16:14:42 -0800

    changed to using faster type as well as writing code to pass tests.

--- src/mmm_c.c
+++ src/mmm_c.c
@@ -3,30 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-U16_DMAT DU16_NewZeros(uint16_t rows, uint16_t cols) {
-  U16_DMAT mat;
-  mat.rows = rows;
-  mat.cols = cols;
-
-  if (DU16_AllocateMemory(&mat) != Ok) {
-    mat.cells = NULL;
-  }
+U16_DMAT DU16_New(uint16_t rows, uint16_t cols) {
+  U16_DMAT mat = {rows, cols, NULL, false};
 
   return mat;
 }
 
+U16_DMAT DU16_NewZeros(uint16_t rows, uint16_t cols) {
+  U16_DMAT mat;
+  mat.rows = rows;
+  mat.cols = cols;
+  mat.cells = NULL;
+
+  if (DU16_AllocateMemory(&mat) != Ok) {
+    mat.is_allocated = false;
+  } else {
+    mat.is_allocated = true;
+  }
+  return mat;
+}
+
 Result DU16_AllocateMemory(U16_DMAT *mat) {
-  if (!calloc(mat->cols * mat->rows, sizeof(uint16_t))) {
+
+  mat->cells =
+      (uint_fast16_t *)malloc(sizeof(uint_fast16_t) * mat->cols * mat->rows);
+  if (!mat->cols) {
+    mat->is_allocated = false;
     return Error;
   }
+
+  mat->is_allocated = true;
   return Ok;
 }
 
 Result DU16_FreeMatrix(U16_DMAT *mat) {
-  if (mat->cells == NULL) {
+  if (!mat->is_allocated) {
+    return Error;
+  } else if (mat->cells == NULL) {
+    mat->is_allocated = false;
     return Error;
   }
 
   free(mat->cells);
+  mat->cells = NULL;
+  mat->is_allocated = false;
   return Ok;
 }
