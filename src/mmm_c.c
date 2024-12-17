fa34406c985ef4a2d40d98672f68b1f6ff31b3ef
jake <jakegoodwin@tutanota.com>
2024-12-16 19:23:21 -0800

    Wrote function body for dot product.
    Also added function body for printing out a matrix.

--- src/mmm_c.c
+++ src/mmm_c.c
@@ -1,7 +1,27 @@
 #include "mmm_c.h"
-#include <alloca.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+Result DU16_PrintMatrix(U16_DMAT *mat) {
+  printf("matrix: shape(r, c):");
+  printf("%d, %d\n", mat->rows, mat->cols);
+
+  for (int i = 0; i < mat->cols; i++) {
+    printf("Column[%d]:\n[\n", i);
+    for (int j = 0; j < mat->rows; j++) {
+      uint_fast16_t *cptr = DU16_CellIndex(mat, j, i);
+
+      if (!cptr) {
+        return Error;
+      }
+
+      printf("%d,\n", *cptr);
+    }
+    printf("]\n");
+  }
+
+  return Ok;
+}
 
 U16_DMAT DU16_New(unsigned int rows, unsigned int cols) {
   U16_DMAT mat = {rows, cols, NULL, false};
@@ -82,6 +102,21 @@ Result DU16_MultiplyByNum(U16_DMAT *mat, uint16_t value) {
 Result DU16_MultiplyInPlace(U16_DMAT *mata, U16_DMAT *matb) { return Ok; }
 
 U16_DMAT DU16_DotProduct(U16_DMAT *mata, U16_DMAT *matb) {
-  U16_DMAT mat_result = DU16_New(0, 0);
+  U16_DMAT mat_result = DU16_New(mata->rows, matb->cols);
+  DU16_AllocateMemory(&mat_result);
+  DU16_SetAllTo(&mat_result, 0);
+
+  for (int i = 0; i < matb->cols; i++) {
+    for (int j = 0; j < mata->rows; j++) { // Shared column/row value
+      uint_fast16_t *cellr = DU16_CellIndex(&mat_result, i, j);
+      *cellr = 0;
+      for (int k = 0; k < mata->cols; k++) {
+        uint_fast16_t *cella = DU16_CellIndex(mata, i, k);
+        uint_fast16_t *cellb = DU16_CellIndex(matb, k, j);
+        *cellr += (*cella) * (*cellb);
+      } // K Loop End
+    }   // J Loop End
+  }     // I Loop End
+
   return mat_result;
 }
